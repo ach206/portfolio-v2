327857482092483898eb6712d14a587da4110554
Cami <camille@creativeambition.net>
2019-03-12 04:40:29 -0700

    continue on animejs

--- src/comps/TileBW.js
+++ src/comps/TileBW.js
@@ -9,7 +9,10 @@ class TileBW extends Component {
                     <div className="bottom-tile">
                     </div>
                     <div className="top-tile">
-                        <h3 className={this.props.title}>{this.props.title}</h3>
+                        <div className="tile_title_cntr">
+                        <h2 className={this.props.title} id={this.props.title + "1"}>{this.props.title}</h2>
+                        <h2 className={this.props.title} id={this.props.title + "2"}>{this.props.title}</h2>
+                        </div>
                         <a href="/">
                         <i className="material-icons md-48 hvr-float-shadow">{this.props.icon}</i>
                         </a>
