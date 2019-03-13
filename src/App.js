37f17c82e93444744048ab4222c764177de6fe8d
Cami <camille@creativeambition.net>
2019-03-13 00:06:46 -0700

    updating npm module for bug fix on img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import TypingAni from './comps/TypingAni';
-import logo from './images/afro2.jpg';
+import logo from './images/braidout-black-logo.png';
 import TileBW from './comps/TileBW';
+import About from './comps/About';
+import styled from 'styled-components';
+import BraidoutHair from './images/braidout-hair.png';
+import BraidoutFace from './images/braidout-face.png';
+import ErrorBoundary from './comps/ErrorBoundary'
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ErrorBoundary>
         <div className="header">
         <img src={logo} alt="logo" id={logo}/>
       <TypingAni />
@@ -17,7 +24,15 @@ class App extends Component {
       <TileBW tileID={1} title={"about"} icon={"fingerprint"} asset={"inactive-asset"}/>
       <TileBW tileID={2} title={"gallery"} icon={"important_devices"} asset={"inactive-asset"}/>
       <TileBW tileID={3} title={"resume"} icon={"description"} asset={"triangle"} />
+       <Hair>
+        <img src={BraidoutHair} alt="logo" id="BraidoutHair"/>
+       </Hair>
+     <Face>
+        <img src={BraidoutFace} alt="logo" id="BraidoutFace"/>
+     </Face>
+      <About />
         </div>
+        </ErrorBoundary>
 
       </div>
     );
@@ -25,3 +40,17 @@ class App extends Component {
 }
 
 export default App;
+
+const Face = styled.img`
+transition: transform 3s;  
+:hover {
+  transform: scale(2);
+}
+`;
+const Hair = styled.img`
+transition: transform 3s;
+:hover {
+  transform: scale(2);
+}
+`;
+
--- src/comps/TileBW.js
+++ src/comps/TileBW.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class TileBW extends Component {
     render() {
         return (
+            <Tile>
+
             <div className="tileCntr" id={this.props.tileID}>
                         <div className={this.props.asset}></div>    
                 <div className="tiles">
@@ -10,8 +13,8 @@ class TileBW extends Component {
                     </div>
                     <div className="top-tile">
                         <div className="tile_title_cntr">
-                        <h2 className={this.props.title} id={this.props.title + "1"}>{this.props.title}</h2>
-                        <h2 className={this.props.title} id={this.props.title + "2"}>{this.props.title}</h2>
+                        <h3 className={this.props.title} id={this.props.title + "1"}>{this.props.title}</h3>
+                        <h3 className={this.props.title} id={this.props.title + "2"}>{this.props.title}</h3>
                         </div>
                         <a href="/">
                         <i className="material-icons md-48 hvr-float-shadow">{this.props.icon}</i>
@@ -19,9 +22,19 @@ class TileBW extends Component {
                     </div>
                 </div>
             </div>
+            </Tile>
         );
     }
 }
 
 export default TileBW;
 
+const Tile = styled.div`
+display: flex;
+position: relative;
+transform: matrix(1, -0.04, -0.05, 1, 80, 80);
+transition: transform 1s;
+:hover {
+        transform: rotate(-2deg);
+    }
+`;
