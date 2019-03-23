1f226f9169b28fc177e8fd81bc069eccea7b30b1
Cami <camille@creativeambition.net>
2019-03-23 16:17:56 -0700

    content for about me

---
+++ src/comps/BlockText.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class BlockText extends Component {
+    render() {
+        return (
+            <div>
+                <blockquote>
+                    I've found myself learning not to accept no for an answer
+                    &mdash;at least not the <em>first</em> no. When I think of
+                    pioneers who've paved the way in front of me I have to think
+                    about all the times they've heard "no" or better yet "you can't".
+                    That answer is just unacceptable. <strong>I can. I will.</strong>
+                    Together we can start dissolving these old ways of thinking and
+                    shift our energy towards collective development for a better future.
+                    
+                 </blockquote>
+            </div>
+        );
+    }
+}
+
+export default BlockText;
