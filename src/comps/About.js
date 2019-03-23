ef59ba020444143f9ad62b90362fbaa1e246d390
Cami <camille@creativeambition.net>
2019-03-23 16:03:07 -0700

    updated bio

--- src/comps/About.js
+++ src/comps/About.js
@@ -4,29 +4,30 @@ class About extends Component {
     render() {
         return (
             <div className="about_section">
-                <h2>ABOUT</h2>
-                < p > Camille Hall is a Seattle -
-                 based Web + UI developer.
-                 A graduate of Arizona State 
-                 University Polytechnic with a 
-                 Bachelors of Science in Graphic 
-                 Information Technology then going 
-                 on to be accepted into Google 's 
-                 competitive Grow With Me program by 
-                 way of the 
-                Udacity Front-End Web Development 
-                nanodegree.</p>
-                < span > FRONT - END WEB DEVELOPER.
-                UX UI ENTHUSIAST.WRITER.ARTIST.
-                LIFE DESIGNER. </span>
-                < p > Camille focuses her craft in 
-                planning a dynamic online presence
-                for her clients then turning 
-                that vision into scaleable code.
-                Outside of work, Camille manages her 
-                own lifestyle 
-                blog and a community contributor on 
-                Thought Catalog. < /p>
+                <p>Hello</p>
+                <p>
+                    I am Camille Hall. I am a Seattle based full-stack
+                    developer. I enjoy contributing to projects that
+                    allow me to work in cross-functional teams and promote
+                    autonomy within my position.
+                </p>
+                <p>Growth</p>
+                <p>
+                    Here's a trick that took me some time to master...
+                    let's see if you already know it. 
+                        <span>We are constantly in growth.</span>
+                </p>
+                <p>Goals AF</p>
+                <p>
+                    Fun fact: I want to travel the world speaking at tech
+                    \& design conferences and get paid to help others break
+                    into tech. 
+                </p>
+                <h3>TL;DR</h3>
+                <p>Name: Camille Hall</p>
+                <p>Location: Greater Seattle area</p>
+                <p>For Hire: Open to Frontend and UI Development*</p>
+                <span><em>*willing to relocate</em></span>
             </div>
         );
     }
