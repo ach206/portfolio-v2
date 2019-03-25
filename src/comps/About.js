5e9afad7bbad0408dc26eeec16218ec623f72158
Cami <camille@creativeambition.net>
2019-03-24 23:27:05 -0700

    updated text content

--- src/comps/About.js
+++ src/comps/About.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import CamilleImg from './../images/DSC3135.jpg';
+
 
 class About extends Component {
     render() {
         return (
-            <div className="about_section">
-                <p>Hello</p>
-                <p>
-                    I am Camille Hall. I am a Seattle based full-stack
+        <div className="about_section">
+        <img src={CamilleImg} alt="Camille" class="about-img"/>
+             <div className="about_section_text">
+                <p>Hello
+                <br/>
+                    I am Camille Hall. I'm a Seattle based full-stack
                     developer. I enjoy contributing to projects that
-                    allow me to work in cross-functional teams and promote
+                    allow me to work in cross-functional teams and encourage
                     autonomy within my position.
                 </p>
-                <p>Growth</p>
-                <p>
+                <p>Growth
+                    <br/>
                     Here's a trick that took me some time to master...
                     let's see if you already know it. 
                         <span>We are constantly in growth.</span>
                 </p>
-                <p>Goals AF</p>
-                <p>
+                <p>Goals AF
+                    <br/>
                     Fun fact: I want to travel the world speaking at tech
-                    \& design conferences and get paid to help others break
+                    & design conferences and get paid to help others break
                     into tech. 
                 </p>
-                <h3>TL;DR</h3>
-                <p>Name: Camille Hall</p>
-                <p>Location: Greater Seattle area</p>
-                <p>For Hire: Open to Frontend and UI Development*</p>
-                <span><em>*willing to relocate</em></span>
+                <p>TL;DR <br/>
+                <strong>Name</strong>: Camille Hall<br/>
+                <strong>Location</strong>: Greater Seattle area<br/>
+                <strong>For Hire</strong>: Open to Frontend and UI Development*</p>
+                <span class="about_memo"><em>*willing to relocate</em></span>
             </div>
+        </div>
         );
     }
 }
 
 export default About;
+
+const AboutImg = styled.div `
+    
+`;
+
+const AboutSection = styled.div `
+    
+`;
