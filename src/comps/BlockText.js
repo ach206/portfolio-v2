0da19db574056cf5d150a23ec17dc0684e239f1e
Cami <camille@creativeambition.net>
2019-03-24 23:25:53 -0700

    updated default styles

--- src/comps/BlockText.js
+++ src/comps/BlockText.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 
 class BlockText extends Component {
     render() {
         return (
             <div>
+                <BlockCntr>
+
                 <blockquote>
                     I've found myself learning not to accept no for an answer
                     &mdash;at least not the <em>first</em> no. When I think of
@@ -14,9 +18,20 @@ class BlockText extends Component {
                     shift our energy towards collective development for a better future.
                     
                  </blockquote>
+                </BlockCntr>
             </div>
         );
     }
 }
 
 export default BlockText;
+
+const BlockCntr = styled.div`
+    max-width: 300px;
+    text-align: justify;
+    font-family: monospace;
+    font-weight: 700;
+    position: initial;
+    color: rgb(128, 128, 128);
+    color: rgba(128, 128, 128, .8);
+`;
