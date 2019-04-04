42c3e239bc39d41fd3401ce2351a1945aa7211da
Camille Hall <camille@creativeambition.net>
2019-04-04 02:11:05 -0700

    updated content

--- src/comps/About.js
+++ src/comps/About.js
@@ -7,32 +7,30 @@ class About extends Component {
     render() {
         return (
         <div className="about_section">
-        <img src={CamilleImg} alt="Camille" class="about-img"/>
+        <img src={CamilleImg} alt="Camille" className="about-img"/>
              <div className="about_section_text">
-                <p>Hello
+                <p><span>Hello</span>
                 <br/>
                     I am Camille Hall. I'm a Seattle based full-stack
                     developer. I enjoy contributing to projects that
                     allow me to work in cross-functional teams and encourage
                     autonomy within my position.
                 </p>
-                <p>Growth
+                <p><span>Growth</span>
                     <br/>
-                    Here's a trick that took me some time to master...
-                    let's see if you already know it. 
-                        <span>We are constantly in growth.</span>
+                    From Designer to Developer—One of the greatest things I learned after high school was 
+                     that I cannot wait for opportunities to come to me but rather I had to 
+                     create my own successes. Go after what I want or create my own lane! My path 
+                     was not at all smooth; it came with many setbacks and few but measurable successes. 
+                     One of the most valuable things I can do now is to 
+                    continue to defy all odds just as many of the women in tech have done in the past.
                 </p>
-                <p>Goals AF
+                <p><span>Goals</span>
                     <br/>
-                    Fun fact: I want to travel the world speaking at tech
+                    I want to travel the world speaking at tech
                     & design conferences and get paid to help others break
                     into tech. 
                 </p>
-                <p>TL;DR <br/>
-                <strong>Name</strong>: Camille Hall<br/>
-                <strong>Location</strong>: Greater Seattle area<br/>
-                <strong>For Hire</strong>: Open to Frontend and UI Development*</p>
-                <span class="about_memo"><em>*willing to relocate</em></span>
             </div>
         </div>
         );
