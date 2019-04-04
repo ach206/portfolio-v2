a63fe5571fb7614907ff3b5e219793db76d10155
Camille Hall <camille@creativeambition.net>
2019-04-04 02:10:39 -0700

    click evt on pdf

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react';
-import './App.css';
-import TypingAni from './comps/TypingAni';
-import logo from './images/braidout-black-logo.png';
-import TileBW from './comps/TileBW';
-import About from './comps/About';
-import styled from 'styled-components';
-import BraidoutHair from './images/braidout-hair.png';
-import BraidoutFace from './images/braidout-face.png';
-import ErrorBoundary from './comps/ErrorBoundary'
-import Triangle from './comps/Triangle'
+// import React, { Component } from 'react';
+// import './App.css';
+// import TypingAni from './comps/TypingAni';
+// import logo from './images/braidout-black-logo.png';
+// import TileBW from './comps/TileBW';
+// import About from './comps/About';
+// import styled from 'styled-components';
+// import BraidoutHair from './images/braidout-hair.png';
+// import BraidoutFace from './images/braidout-face.png';
+// import ErrorBoundary from './comps/ErrorBoundary'
+// import Triangle from './comps/Triangle'
 
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ErrorBoundary>
-        <div className="header">
-        <img src={logo} alt="logo" id={logo}/>
-      <TypingAni />
-        </div>
-        <div className="tiles-cntr">
-        <Triangle />
-      <TileBW tileID={1} title={"about"} icon={"fingerprint"} asset={"inactive-asset"} bkg={"../images/mint-lav-3-x5.png"} />
-      <TileBW tileID={2} title={"gallery"} icon={"important_devices"} asset={"inactive-asset"} bkg={"../images/mint-lav-6-x5.png"} />
-      <TileBW tileID={3} title={"resume"} icon={"description"} asset={"triangle"}  bkg={"../images/mint-lav-3-x5.png"} />
-       <Hair>
-      <img src={BraidoutHair} alt="logo" id={BraidoutHair}/>
-       </Hair>
-     <Face>
-        <div><img src={BraidoutFace} alt="logo" id={BraidoutFace}/></div>
-     </Face>
-      <About />
-        <Triangle />
-        </div>
-        </ErrorBoundary>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <ErrorBoundary>
+//         <div className="header">
+//         <img src={logo} alt="logo" id={logo}/>
+//       <TypingAni />
+//         </div>
+//         <div className="tiles-cntr">
+//         <Triangle />
+//       <TileBW tileID={1} title={"about"} icon={"fingerprint"} asset={"inactive-asset"} bkg={"../images/mint-lav-3-x5.png"} />
+//       <TileBW tileID={2} title={"gallery"} icon={"important_devices"} asset={"inactive-asset"} bkg={"../images/mint-lav-6-x5.png"} />
+//       <TileBW tileID={3} title={"resume"} icon={"description"} asset={"triangle"}  bkg={"../images/mint-lav-3-x5.png"} />
+//        <Hair>
+//       <img src={BraidoutHair} alt="logo" id={BraidoutHair}/>
+//        </Hair>
+//      <Face>
+//         <div><img src={BraidoutFace} alt="logo" id={BraidoutFace}/></div>
+//      </Face>
+//       <About />
+//         <Triangle />
+//         </div>
+//         </ErrorBoundary>
 
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
 
-const Face = styled.div`
-transition: transform 3s;  
-:hover {
-  transform: scale(2);
-}
-`;
-const Hair = styled.div`
-transition: transform 3s;
-`;
-  // :hover {
-  //   transform: scale(2);
-  // }
+// const Face = styled.div`
+// transition: transform 3s;  
+// :hover {
+//   transform: scale(2);
+// }
+// `;
+// const Hair = styled.div`
+// transition: transform 3s;
+// `;
+//   // :hover {
+//   //   transform: scale(2);
+//   // }
 
