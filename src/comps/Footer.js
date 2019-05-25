d9e8b0651f7d24743250872571e7d3840854f069
Camille Hall <camille@creativeambition.net>
2019-05-24 22:53:55 -0700

    updated content

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -11,6 +11,11 @@ class Footer extends Component {
             <li className="socialIcons"><a href="https://www.instagram.com/camillehall.js/" target="_blank" rel="noopener noreferrer" title="instagram"><span className="fab fa-instagram fa-2x"></span></a></li>
             <li className="socialIcons"><a href="https://www.twitter.com/camillehall12" target="_blank" rel="noopener noreferrer" title="twitter"><span className="fab fa-twitter-square fa-2x"></span></a></li>
         </ul>
+        <p className="footer-disclaimer">
+          Made with &#10084; from Seattle <br/>
+Designed &#38; Developed by Camille Hall<br/><br/>
+            2015-2019 Creative Ambition&#8482; 
+            </p>
             </div>
         );
     }
