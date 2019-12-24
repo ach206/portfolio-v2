957c69371bdcd660814833b96ec9d1f5b4a5aafc
Camille Hall <camille@creativeambition.net>
2019-12-24 04:48:10 -0600

    updated featured slide

--- src/App.js
+++ src/App.js
@@ -11,14 +11,6 @@ import SlideWrapper from "./comps/SlideWrapper";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 class App extends React.Component {
-  state = {
-    readMoreBtnVisible: true,
-  };
-  constructor(props) {
-    super(props);
-    this.showHide = this.showHide.bind(this);
-
-  }
 
   displayLivePdf = () => {
     window.open(resumePdfView, "_blank");
@@ -83,10 +75,14 @@ class App extends React.Component {
                 </div>
                 <div className="section feature">
           <div className="slide" data-anchor="slide1">
+            <h1>Appearances</h1>
           <iframe title="GitHub Universe Interview with Camille" width="560" height="315" src="https://www.youtube.com/embed/WSi_GxfCCA8?start=3811" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
               <h2>Want more? Check out my interview from <a href="https://www.youtube.com/watch?v=WSi_GxfCCA8&t=63m30s" target="_blank" rel="noopener noreferrer"> GitHub Universe 2018</a></h2>
             </div>
           <div className="slide" data-anchor="slide2">
+            <h2>Features</h2>
+            <p><a href="https://twitter.com/frauvis/status/1205170701314990085" target="_blank" rel="noopener noreferrer" className="feature-links">Frauvis Issue #43</a></p>
+           <p><a href="https://youtu.be/qHDmTVbWJqA?t=480" target="_blank" rel="noopener noreferrer" className="feature-links">2019 Udacity Graduation</a></p>
             <h2>Written Pieces</h2>
             <p><a href="https://medium.com/@camille_66528/i-know-why-companies-arent-hiring-junior-developers-73efcb9f4ede" target="_blank" rel="noopener noreferrer" className="feature-links">I know why companies aren’t hiring junior developers.</a></p>
            <p> <a href="https://www.linkedin.com/pulse/my-first-github-universe-experience-camille-hall/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_post_details%3BiBupD6jcQC6JK2%2Fep959Dg%3D%3D" target="_blank" rel="noopener noreferrer" className="feature-links">My First GitHub Universe Experience</a></p>
