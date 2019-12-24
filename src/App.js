1f1b3883615fd63f1b7bd7553a23d29d7fad3609
Camille Hall <camille@creativeambition.net>
2019-12-24 02:10:07 -0600

    refactor successful

--- src/App.js
+++ src/App.js
@@ -1,59 +1,93 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import TypingAni from './comps/TypingAni';
-// import logo from './images/braidout-black-logo.png';
-// import TileBW from './comps/TileBW';
-// import About from './comps/About';
-// import styled from 'styled-components';
-// import BraidoutHair from './images/braidout-hair.png';
-// import BraidoutFace from './images/braidout-face.png';
-// import ErrorBoundary from './comps/ErrorBoundary'
-// import Triangle from './comps/Triangle'
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+import { Document, Page, pdfjs } from "react-pdf";
+import "./App.css";
+import { TypingAni, About, BlockText, Footer } from "./comps/";
+import { logo, ApriImg, BODImg, CSClubImg, EdenSavvyImg, FroggerImg, FullmoonReleaseImg, GirlBombImg, GitHubImg, KdramaImg, KdramaFbImg, matchGameImg
+} from "./images";
+import { resumePdfView, GalleryData } from "./data/";
+import SlideWrapper from "./comps/SlideWrapper";
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+let displayLivePdf = () => {
+  window.open(resumePdfView, "_blank");
+};
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <ErrorBoundary>
-//         <div className="header">
-//         <img src={logo} alt="logo" id={logo}/>
-//       <TypingAni />
-//         </div>
-//         <div className="tiles-cntr">
-//         <Triangle />
-//       <TileBW tileID={1} title={"about"} icon={"fingerprint"} asset={"inactive-asset"} bkg={"../images/mint-lav-3-x5.png"} />
-//       <TileBW tileID={2} title={"gallery"} icon={"important_devices"} asset={"inactive-asset"} bkg={"../images/mint-lav-6-x5.png"} />
-//       <TileBW tileID={3} title={"resume"} icon={"description"} asset={"triangle"}  bkg={"../images/mint-lav-3-x5.png"} />
-//        <Hair>
-//       <img src={BraidoutHair} alt="logo" id={BraidoutHair}/>
-//        </Hair>
-//      <Face>
-//         <div><img src={BraidoutFace} alt="logo" id={BraidoutFace}/></div>
-//      </Face>
-//       <About />
-//         <Triangle />
-//         </div>
-//         </ErrorBoundary>
+let buildGallery = (GalleryData) => {
+  let images = [ApriImg, BODImg, CSClubImg, EdenSavvyImg, FroggerImg, FullmoonReleaseImg, GirlBombImg, GitHubImg, KdramaImg, KdramaFbImg, matchGameImg]
+  let jsx = []
+  GalleryData.map((data, i) => {
+    jsx.push( <SlideWrapper key={data.id} name={data.name} img={images[i]} url={data.url} stack={data.stack} description={data.description} index={i} /> )
+  })
+  return jsx
+}
 
-//       </div>
-//     );
-//   }
-// }
+const anchors = ["Home", "About", "Gallery", "Press", "Resume"];
+const Fullpage = () => (
+  <ReactFullpage
+    licenseKey= {'wobwH@p8'}
+    anchors={anchors}
+    navigation
+    sectionsColor={["#ffffff", "#ffffff", "#ffffff", "gold", "#ffffff"]}
+    navigationTooltips={anchors}
+    css3={true}
+    autoScrolling={true}
+    fadingEffect={true}
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section home fp-auto-height-responsive">
+            <img src={logo} alt="logo" className="home-img" />
+            <TypingAni />
+          </div>
+          <div className="section about fp-auto-height-responsive">
+            <div className="about">
+              <BlockText />
+              <About />
+            </div>
+          </div>
+          <div className="section gallery fp-auto-height-responsive">
+            { buildGallery(GalleryData)} 
+          </div> 
+          <div className="section feature">
+            <img className="gallery-img" data-src={GitHubImg} alt="gallery" />
+            <h2>
+              Want more? Check out my interview from{" "}
+              <a
+                href="https://www.youtube.com/watch?v=WSi_GxfCCA8&t=63m30s"
+                target="_blank"
+              >
+                {" "}
+                GitHub Universe 2018
+              </a>
+            </h2>
+          </div>
+          <div className="section contact">
+            <div className="contactCntr">
+              <div className="contact-right-aside">
+                <div className="LI-profile-badge" data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="ach206">
+                  <a className="LI-simple-link" href="https://www.linkedin.com/in/ach206?trk=profile-badge" SameSite={"None"}>
+                    Camille Hall
+                  </a>
+                </div>
+                <Footer />
+              </div>
+              <Document
+                file={resumePdfView}
+                className="pdf-doc hvr-pulse-grow"
+                onClick={() => {
+                  displayLivePdf();
+                }}
+              >
+                <Page pageNumber={1} />
+              </Document>
+            </div>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
-// export default App;
-
-// const Face = styled.div`
-// transition: transform 3s;  
-// :hover {
-//   transform: scale(2);
-// }
-// `;
-// const Hair = styled.div`
-// transition: transform 3s;
-// `;
-//   // :hover {
-//   //   transform: scale(2);
-//   // }
-
+export default Fullpage
--- src/comps/SlideWrapper.js
+++ src/comps/SlideWrapper.js
@@ -1,39 +1,36 @@
 import React, { Component } from "react";
 
 export default class SlideWrapper extends Component {
+ 
+  listItems = (items) => {
+    items.map((item, index) => {
+      return <li key={index} className="list-stack-item">{item}</li>
+    })
+  }
+
   render() {
-    const { project } = this.props
+    const { name="Unknown", id="id123", img="", url="", stack=[], description="", index=0 } = this.props
     return (
-      <div className="slide" data-anchor="slide3">
+      <div className="slide" data-anchor={`slide${index+1}`}>
         <div className="gallery-cntr">
           <div className="gallery-content">
-            <h3>{project}</h3>
-            
+            <h3>{name}</h3>
+
             <ul className="list-stack">
               <li className="list-stack-item list-title">Stack</li>
-              <li className="list-stack-item">PHP</li>
-              <li className="list-stack-item">HTML/CSS/JS</li>
-              <li className="list-stack-item">MySQL</li>
-              <li className="list-stack-item">Google Calendar API</li>
-              <li className="list-stack-item">Figma</li>
-              <li className="list-stack-item">Git version control</li>
+              {this.listItems(stack)}
             </ul>
-            <p className="gallery-description">
-              This project consisted of engineering a PHP website from
-              conception to deployment for Highline College's Computer Science
-              Club. Highlighted features include a sleek dark-theme design and
-              also an Admin portal which allows administrators to securely
-              modify stored content on the database.{" "}
+            <p className="gallery-description">{description}{" "}
             </p>
             <a
-              href="https://chelan.highline.edu/~ach206/215/Final_Computer_Science_Club/pages/"
+              href={url}
               target="_blank"
               className="gallery-demo-btn"
             >
               See Demo
             </a>
           </div>
-          <img className="gallery-img" src={CSClubImg} alt="gallery" />
+          <img className="gallery-img" src={img} alt="gallery" />
         </div>
       </div>
     );
